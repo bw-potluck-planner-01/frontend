545db7bae47e56f55f645aa2ad9576e75b539116
Leo Hays <starryhornet@gmail.com>
2021-08-23 15:31:02 -0500

    I know it is the worng name for the branch... it is the form for creating a potluck

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 import {Switch, Route} from 'react-router-dom'
+// import axios from 'axios'; //for later
+
+import Pot from './Pot/Pot'
 
 function App() {
+
   return (
     <div className="App">
       {/* Insert Header here */}
@@ -22,7 +26,7 @@ function App() {
         </Route>
         <Route exact path='/potluck'>
           {/* Insert potluck list here */}
-
+          <Pot />
         </Route>
         <Route path='/potluck/id'>
           {/* Insert potluck with item list here */}
