eec419b40b849e4af5cccaa0cdbd591f1af10b75
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-08-24 13:24:36 -0400

    Fixing bugs

--- src/action/LoginAction.js
+++ src/action/LoginAction.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-
-import { connect } from "react-redux";
-
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
