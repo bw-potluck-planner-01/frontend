50b9b36d37683fde3b015e8177e1642422e120e4
nathanstom <nathanjobstom@gmail.com>
2021-08-25 16:37:16 -0700

    Finished an add item feature, other changes. Waiting for a potluck id with items

--- src/components/PotluckPage/PotluckItems.js
+++ src/components/PotluckPage/PotluckItems.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import axiosWithAuth from '../../utils/axiosWithAuth';
+import { useParams } from 'react-router-dom';
+
 
 const PotluckItems = props => {
-    const {items} = props;
+    const {items, potluckId} = props;
+    const params = useParams();
+
+    const [inputField, setInputField] = useState("");
+
+    const inputChange = (e) => {
+        setInputField(e.target.value);
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        axiosWithAuth().post(`https://potluckplannerplus.herokuapp.com/potlucks/${params.id}/menu`, { food_item: inputField})
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
+    const [formData, setFormData] = useState("");
 
     return (
+    <>
+        <form onSubmit={submit}>
+            <label>
+                Add Item 
+                <input onChange={inputChange} id='add' type='text' name='addBar' value={formData.add} placeholder='Add Something'></input>
+            </label>
+        </form>
 
         <div>
             <h3>{items}</h3>
         </div>
-
+    </>
     )
 }
 
--- src/components/PotluckPage/PotluckPage.js
+++ src/components/PotluckPage/PotluckPage.js
@@ -1,41 +1,53 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axiosWithAuth from '../../utils/axiosWithAuth';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import PotluckLocation from './PotluckLocation';
+import PotluckItems from './PotluckItems';
 import './PotluckItems'
 
 
-export default function PotluckPage(){
+export default function PotluckPage() {
     const params = useParams();
     console.log(params);
-    const [potluckInfo, setPotluckInfo] = useState ({
+    const [potluckInfo, setPotluckInfo] = useState({
         potluck_name: '',
         date: '',
         time: '',
         location: ''
     });
 
-    useEffect( () => {
+    const [potluckItems, setPotluckItems] = useState([])
+
+    useEffect(() => {
         console.log(`the id of this page is ${params.id}`)
-        axiosWithAuth().get(`https://potluckplannerplus.herokuapp.com/org/${params.id}/potlucks`)
-        .then(res => {
-            console.log(res.data)
-            setPotluckInfo(res.data)
-        })
-        .catch(err => {
-            console.error(err)
-        })
+        axiosWithAuth().get(`https://potluckplannerplus.herokuapp.com/potlucks/${params.id}`)
+            .then(res => {
+                console.log(res.data)
+                setPotluckInfo(res.data)
+                axiosWithAuth().get(`https://potluckplannerplus.herokuapp.com/potlucks/${params.id}/menu`)
+                    .then(res => {
+                        console.log(res.data)
+                        setPotluckItems(res.data)
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    })
+            })
+            .catch(err => {
+                console.error(err)
+            })
     }, [])
 
-    return(
+    return (
         <div className="PotluckPage">
             <PotluckLocation
                 potluck_name={potluckInfo.potluck_name}
                 date={potluckInfo.date}
                 time={potluckInfo.time}
                 location={potluckInfo.location}
-            />
+            />                                          {/* CHANGE THE 1 TO WHATEVER THE POTLUCK ID IS ASAP */}
+            <PotluckItems items={potluckItems} potluckId={1} />
         </div>
     )
 
